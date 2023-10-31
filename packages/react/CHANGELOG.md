0838c3b672bb42c6ff06cfb7c6c50e082f873c67
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-10-31 16:02:35 -0300

    fix: Ref to TextInput and ajust some styles

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @bernardorh-ignite-ui/react
 
+## 2.0.1
+
+### Patch Changes
+
+- Add some fixes to future projects, options to pass references to TextInput and ajust some styles
+
 ## 2.0.0
 
 ### Major Changes
--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,17 +1,17 @@
-import React, { ComponentProps } from 'react'
+import React, { ComponentProps, forwardRef, ElementRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
 }
 
-export function TextInput({ prefix, ...props }: TextInputProps) {
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(({ prefix, ref, ...props }: TextInputProps) => {
   return (
     <TextInputContainer>
       {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
+      <Input ref={ref} {...props} />
     </TextInputContainer>
   )
-}
+})
 
 TextInput.displayName = 'TextInput'
