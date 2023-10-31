e8eac6df9b287f12b28d1aadfac3260219bba754
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-10-31 14:06:35 -0300

    changing: Name of the package so I can publish it on npm

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@bernardorh-ignite-ui/react'
 
 export default {
   title: 'Form/Button',
